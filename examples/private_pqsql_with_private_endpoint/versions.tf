c1b225ff79fefaf215a3b215cc9b38fccb3ff885
CloudDrove CI <84795582+clouddrove-ci@users.noreply.github.com>
2026-03-20 15:52:19 -0400

    chore: polish module with basic example, changelog, and version fixes (#25)
    Co-authored-by: Anmol Nagpal <anmolnagpal@users.noreply.github.com>

--- examples/private_pqsql_with_private_endpoint/versions.tf
+++ examples/private_pqsql_with_private_endpoint/versions.tf
@@ -1,24 +1,10 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
-}
 
-terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.116.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">=3.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
+      version = "# Terraform version"
     }
   }
 }
