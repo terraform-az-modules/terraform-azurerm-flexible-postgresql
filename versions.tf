9df3e4b84a993b9edd76e3442a68eeaeec6085e8
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-21 08:49:12 -0400

    fix: consolidate versions.tf, remove provider_meta, upgrade to azurerm >= 4.0

--- versions.tf
+++ versions.tf
@@ -1,26 +1,18 @@
-##-----------------------------------------------------------------------------
-## Versions
-##-----------------------------------------------------------------------------
-# Terraform version
 terraform {
   required_version = ">= 1.10.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.116.0"
+      version = ">= 4.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">=3.0.0"
+      version = ">= 3.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0.0"
+      version = ">= 3.6"
     }
-  }
-
-  provider_meta "azurerm" {
-    module_name = "terraform-az-modules/terraform-azurerm-flexible-postgresql"
   }
 }
